e017679561a53c6d2082e1d28b0974c4c52221aa
Vlad Vetshtein <vlad.vetshtein@intel.com>
2026-07-29 08:46:39 +0300

    Re-export load_stack so analysis notebook can import it

--- src/cell_analysis/__init__.py
+++ src/cell_analysis/__init__.py
@@ -31,6 +31,7 @@ from .io import (
     save_results as save_results,
     save_summary as save_summary,
     export_notebook_html as export_notebook_html,
+    load_stack as load_stack,
     ExtractionBundle as ExtractionBundle,
     save_extraction as save_extraction,
     load_extraction as load_extraction,
